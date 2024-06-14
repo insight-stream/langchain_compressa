0ec782259b99ec04fec3b9bbe2f2c942bc189b87
Margarita <kulachenkova@gmail.com>
2024-06-14 12:38:07 +0300

    fix llm

--- langchain_compressa/__init__.py
+++ langchain_compressa/__init__.py
@@ -1,6 +1,5 @@
 from importlib import metadata
 
-from langchain_compressa.chat_models import ChatCompressa
 from langchain_compressa.embeddings import CompressaEmbeddings
 from langchain_compressa.llms import CompressaLLM
 
@@ -12,7 +11,6 @@ except metadata.PackageNotFoundError:
 del metadata  # optional, avoids polluting the results of dir(__package__)
 
 __all__ = [
-    "ChatCompressa",
     "CompressaLLM",
     "CompressaEmbeddings",
     "__version__",
