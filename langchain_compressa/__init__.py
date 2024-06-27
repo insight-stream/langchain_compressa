1019645c0864252a4054c2423b8c17ed07bfa09f
Margarita <kulachenkova@gmail.com>
2024-06-27 09:33:49 +0300

    upd

--- langchain_compressa/__init__.py
+++ langchain_compressa/__init__.py
@@ -2,6 +2,7 @@ from importlib import metadata
 
 from langchain_compressa.embeddings import CompressaEmbeddings
 from langchain_compressa.llms import CompressaLLM
+from langchain_compressa.chat_models import ChatCompressa
 
 try:
     __version__ = metadata.version(__package__)
@@ -13,5 +14,6 @@ del metadata  # optional, avoids polluting the results of dir(__package__)
 __all__ = [
     "CompressaLLM",
     "CompressaEmbeddings",
+    "ChatCompressa",
     "__version__",
 ]
